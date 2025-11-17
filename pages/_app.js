b53c4c1e38639937782e412b74e1745c2ebca947
Teemu eemeli Tupola <Teemu.tupola@student.hamk.fi>
2025-11-17 08:18:34 +0200

    speedInsight vercel

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,15 @@ import "../styles/info.css"
 import "../styles/Footer.css"
 import { LanguageProvider } from "../components/LangChanger.js";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 
 export default function App({ Component, pageProps }) {
   return (
     <LanguageProvider>
      <Analytics />
+      <SpeedInsights />
       <Component {...pageProps} />
     </LanguageProvider>
   );
